cca0a10e153a2e0a71b74c216ca9c0bcdc76bb5e
Ali-Gharsheen <88122412+Ali-Gharsheen@users.noreply.github.com>
2022-06-28 18:35:41 +0500

    06-28-22

--- src/app/car-insurance-details/car-insurance-details.page.ts
+++ src/app/car-insurance-details/car-insurance-details.page.ts
@@ -20,18 +20,49 @@ export class CarInsuranceDetailsPage implements OnInit {
   showPolicyholder = false;
   showVehicleModel = false;
   showVehicleUsage = false;
+  showGender = false;
+  showDay = false;
+  showMonth = false;
+  showYear = false;
 
   vehicleVal = '';
   vehicleMakeVal = '';
   policyhldrVal = '';
   vehicleModelVal = '';
   vehicleUsageVal = 'Personal';
-  
+  genderVal = 'Please Select';
+  dayVal = 'Day';
+  monthVal = 'Month';
+  yearVal = 'Year';
+
+
+  genderType = [{gender: 'Male'},{gender: 'Female'}]
   vehicleClass = [{ vehicle: 'Cars/Sedan/SUV' }]
   vehicleMake = [{ vMake : 'Acura'},{vMake : 'Acura'}]
   policyhldrList = [{ plcyhldrType: 'Private' }, { plcyhldrType:'Corporate' }]
   vehicleModel = [{ vModel : 'EL'},{ vModel : 'EL'}]
   vehicleUsage = [{ usage: 'Personal'},{ usage: 'Personal'}]
+  dateYear = [
+    { year: 1950 }, { year: 1951 }, { year: 1952 }, { year: 1953 },{ year: 1954 },{ year: 1955 },{ year: 1956 },
+    { year: 1957 },{ year: 1958 },{ year: 1959 },{ year: 1960 },{ year: 1961 },{ year: 1962 },{ year: 1963 },
+    { year: 1964 },{ year: 1965 },{ year: 1966 },{ year: 1967 },{ year: 1968 },{ year: 1969 },{ year: 1970 },
+    { year: 1971 },{ year: 1972 },{ year: 1973 }, { year: 1974 }, { year: 1975 }, { year: 1976 },{ year: 1977 },
+    { year: 1978 },{ year: 1979 },{ year: 1980 },{ year: 1981 },{ year: 1982 },{ year: 1983 },{ year: 1984 },
+    { year: 1985 },{ year: 1986 },{ year: 1987 },{ year: 1988 },{ year: 1989 },{ year: 1990 },{ year: 1991 },
+    { year: 1992 },{ year: 1993 },{ year: 1994 },{ year: 1995 },{ year: 1996 },{ year: 1997 },{ year: 1998 },
+    { year: 1999 },{ year: 2000 },{ year: 2001 },{ year: 2002 },{ year: 2003 },{ year: 2004 },{ year: 2005 },
+    { year: 2006 },{ year: 2007 },{ year: 2008 },{ year: 2009 },{ year: 2010 },{ year: 2011 },{ year: 2012 },
+    { year: 2013 },{ year: 2014 },{ year: 2015 },{ year: 2016 },{ year: 2017 },{ year: 2018 },{ year: 2019 },
+    { year: 2020 },{ year: 2021 },{ year: 2022 },{ year: 2023 }
+  ];
+  dateDay=[{day : 1},{day : 2},{day : 3},{day : 4},{day : 5},{day : 6},{day : 7},{day : 8},{day : 9},{day : 10},
+    {day : 11},{day : 12},{day : 13},{day : 14},{day : 15},{day : 16},{day : 17},{day : 18},{day : 19},{day : 20},
+    {day : 21},{day : 22},{day : 23},{day : 24},{day : 25},{day : 26},{day : 27},{day : 28},{day : 29},{day : 30},
+    {day : 31}
+  ]
+  dateMonth=[{month:'01'},{month:'02'},{month:'03'},{month:'04'},{month:'05'},{month:'06'},{month:'07'},
+    {month:'08'},{month:'09'},{month:'10'},{month:'11'},{month:'12'},
+  ]
   constructor(public router:Router,
     public api:InsuranceAppService) { }
 
@@ -48,75 +79,63 @@ export class CarInsuranceDetailsPage implements OnInit {
     this.mobNumber = '';
     this.compName = '';
   }
-  openVehicleList() {
-    if (this.showVehicle == true) {
-      this.showVehicle = false
+
+  openGenderList(){
+    if (this.showGender == true) {
+      this.showGender = false
     } else {
-      this.showVehicle = true;
+      this.showGender = true;
 
     }
   }
 
-  openVhclMakeList() {
-    if (this.showMaker == true) {
-      this.showMaker = false
+  openDayList(){
+    if (this.showDay == true) {
+      this.showDay = false
     } else {
-      this.showMaker = true;
+      this.showDay = true;
 
     }
   }
 
-  openPolicyhldrList(){
-    if (this.showPolicyholder == true) {
-      this.showPolicyholder = false
+  openMonthList(){
+    if (this.showMonth == true) {
+      this.showMonth = false
     } else {
-      this.showPolicyholder = true;
+      this.showMonth = true;
 
     }
   }
 
-  opnenVehicleModelList(){
-    if (this.showVehicleModel == true) {
-      this.showVehicleModel = false
+  openYearList(){
+    if (this.showYear == true) {
+      this.showYear = false
     } else {
-      this.showVehicleModel = true;
+      this.showYear = true;
 
     }
-  }
-
-  opnenVehicleUsageList(){
-    if (this.showVehicleUsage == true) {
-      this.showVehicleUsage = false
-    } else {
-      this.showVehicleUsage = true;
-
-    }
-  }
-
-  selectVehicleCls(list) {
-    this.vehicleVal= list.vehicle;
-    this.showVehicle = false;
-  }
-
-  selectVehicleMaker(list){
-    this.vehicleMakeVal = list.vMake;
-    this.showMaker = false;
-  }
-
-  selectPolicyholder(list){
-    this.policyhldrVal = list.plcyhldrType;
-    this.showPolicyholder = false;
   }
   
-  selectVehicleModel(list){
-    this.vehicleModelVal = list.vModel;
-    this.showVehicleModel = false;
-  }
-  selectVehicleUsage(list){
-    this.vehicleUsageVal = list.usage;
-    this.showVehicleUsage = false;
+  selectGenderType(list){
+    this.genderVal= list.gender;
+    this.showGender = false;
   }
 
+  selectDay(list){
+    this.dayVal= list.day;
+    this.showDay = false;
+  }
+
+  selectMonth(list){
+    this.monthVal= list.month;
+    this.showMonth = false;
+  }
+
+  selectYear(list){
+    this.yearVal= list.year;
+    this.showYear = false;
+  }
+  
   goback(){
     this.router.navigate(['/home-page-screen-after-login']);
   }
